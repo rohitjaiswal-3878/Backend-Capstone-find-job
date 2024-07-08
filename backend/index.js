cab79f61646bc7856ff958b0bfde3e059a608ff1
rohitjaiswal-3878 <rj710910@gmail.com>
2024-07-08 23:57:18 +0530

    Backend : Password Security, Login, and Middleware and Logging

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,38 @@ const bodyParser = require("body-parser");
 dotenv.config();
 const port = process.env.PORT;
 const authRoutes = require("./routes/auth/");
+const fs = require("fs");
 
 app.use(bodyParser.json());
+
+// log every in coming request.
+// store it in a text file.
+app.use((req, res, next) => {
+  const log = `${req.method} - ${req.url} - ${req.ip} - ${new Date()}`;
+  fs.appendFile("log.txt", log, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  next();
+});
 
 app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// error handling middleware.
+app.use((err, req, res, next) => {
+  let log = err.stack;
+  log += `/n${req.method} - ${req.url} - ${req.ip} - ${new Date()}`;
+  fs.appendFile("error.txt", log, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  res.status(500).json({ message: "Serve down!" });
 });
 
 app.listen(port, () => {
--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../schemas/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: "Wrong email or password." }); // user does not exist.
+    }
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Wromg email or password." });
+    } else {
+      const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+      res
+        .header("auth-token", token)
+        .json({ message: "Logged in successfully!" });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.post("/register", async (req, res) => {
   const { name, email, password, mobile } = req.body;
@@ -8,10 +31,12 @@ router.post("/register", async (req, res) => {
   if (user) {
     return res.status(400).json({ message: "User already exists." });
   }
+  const salt = await bcrypt.genSalt(10); // genSalt adds the level of difficulty of the algorithm
+  const hashPassword = await bcrypt.hash(password, salt);
   const newUser = new User({
     name,
     email,
-    password,
+    password: hashPassword,
     mobile,
   });
   await newUser.save();
