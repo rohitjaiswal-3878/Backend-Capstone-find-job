12b4fbac178554f09ece7ea80cdcf455632f340f
rohitjaiswal-3878 <rj710910@gmail.com>
2024-07-19 01:13:11 +0530

    Backend : Updated cors.

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const authMiddleware = require("./middleware/auth");
 const cors = require("cors");
 app.use(
   cors({
-    origin: "https://backend-capstone-find-job.vercel.app/",
+    origin: "https://backend-capstone-find-job.vercel.app",
     credentials: true,
     exposedHeaders: "auth-token",
     "Access-Control-Allow-Headers": "auth-token",
